a1a3378b80a22d97e377125d809a253a18559a0b
roux <nviet8640@gmail.com>
2021-11-23 22:44:42 +0700

    bai tap ngay 17/11

--- 17.11/e.c
+++ 17.11/e.c
@@ -1,11 +1,24 @@
 #include <stdio.h>
 int main()
 {
-    int n = 0, count = 1;
+    int n = 0, count = 0, ar[100] = {};
     scanf("%d", &n);
-    for (int i = 1; i < (int)(n / 2) + 1; i++)
+
+    for (int i = 2; i <= n; i++)
+    {
         if (n % i == 0)
+        {
             count++;
-    printf("%d", count);
+            while (n % i == 0)
+            {
+                n /= i;
+                ar[count]++;
+            }
+        }
+    }
+    int ans = 1;
+    for (int i = 1; i <= count; i++)
+        ans = ans * (ar[i] + 1);
+    printf("%d", ans);
     return 0;
 }
